145e5b82434c4ee9e1022d24f24ce4dcdca31f46
jesus bryan <181741@utags.edu.mx>
2022-03-20 20:30:33 -0600

    Correccion de chekin y chekout

--- src/Account.js
+++ src/Account.js
@@ -7,20 +7,49 @@ const Account = ({ session }) => {
   const [chekin, setChekin] = useState(null)
   const [chekout, setChekout] = useState(null)
   const [placas, setPlacas] = useState(null)
+  const [visitas, setVisitas] = useState(null)
 
+function hora(){
+    const fecha = new Date();
+    const hora = fecha.getHours();
+    const min = fecha.getMinutes();
+    const seg = fecha.getSeconds();
 
+    const horaActual = hora+':'+min+':'+seg;
+    return horaActual;
+}
+function horaPariking() {
+//Conversion de chekin para resta
+    var cadena = chekin;
+    let result1 = cadena.replace(":", "");
+    let result2 = result1.replace(":","");
+console.log(result2); 
+//Conversion de chekin para resta
+var cadena2 = chekout;
+let res1 = cadena2.replace(":", "");
+let res2 = res1.replace(":","");
+console.log(res2); 
+
+    const hora1 = parseFloat(res2);
+    const hora2 = parseFloat(result2);
+    const resultado = hora1-hora2;
+    console.log("el resutado " + resultado );
+    return resultado;
+ 
+}
   //Usuarios
   useEffect(() => {
     getProfile()
   }, [session])
 
   const getProfile = async () => {
+      
     try {
       setLoading(true)
       const user = supabase.auth.user()
       let { data, error, status } = await supabase
         .from('profiles')
-        .select(`username, chekin, placas`)
+        .select(`username, chekin, chekout, placas, visitas`)
         .eq('id', user.id)
         .single()
 
@@ -31,31 +60,9 @@ const Account = ({ session }) => {
       if (data) {
         setUsername(data.username)
         setChekin(data.chekin)
-        setPlacas(data.placas)
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-      setLoading(false)
-    }
-  }
-  //chekout
-  const getChekout = async () => {
-    try {
-      setLoading(true)
-      const user = supabase.auth.user()
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`chekout`)
-        .eq('id', user.id)
-        .single()
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-      if (data) {
         setChekout(data.chekout)
+        setPlacas(data.placas)
+        setVisitas(data.visitas)
       }
     } catch (error) {
       alert(error.message)
@@ -66,17 +73,16 @@ const Account = ({ session }) => {
 //Actualizacion de usuarios (Alta)
   const updateProfile = async (e) => {
     e.preventDefault()
-
     try {
       setLoading(true)
       const user = supabase.auth.user()
-
       const updates = {
         id: user.id,
         username,
-        chekin: new Date(),
+        chekin: hora(),
         placas,
-        chekout: new Date(),
+        chekout,
+        visitas,
         updated_at: new Date(),
       }
 
@@ -93,6 +99,7 @@ const Account = ({ session }) => {
       setLoading(false)
     }
   }
+  
 
   return (
     <div aria-live="polite">
@@ -106,27 +113,46 @@ const Account = ({ session }) => {
             <input
               id="username"
               type="text"
-              value={username || ''}
+              value={username || 'Ingrese su nombre'}
               onChange={(e) => setUsername(e.target.value)}
             />
             <label htmlFor="username">Numero de placas</label>
             <input
               id="username"
               type="text"
-              value={placas || ''}
+              value={placas || 'Ingrese su numero de placas'}
               onChange={(e) => setPlacas(e.target.value)}
             />
 
-          <div> Hora de entrada: {chekin || 'De click en el boton verde para comenzar su tiempo'}</div>
-
-          <div> Hora de salida: {chekout || 'De click en el boton rojo para finalizar su tiempo'}</div>
+          <div> Hora de entrada: (Ingrese con el formato HH:MM:SS){chekin || 'Ingrese su hora de llegada y de click en el boton verde'}</div>
+          <input
+          maxLength={8}
+              id="username"
+              type="text"
+              value={chekin || ''}
+              onChange={(e) => setChekin(e.target.value)}
+            />
+          <div> Hora de salida: (Ingrese con el formato HH:MM:SS) {chekout || 'Ingrese su hora de salida y de click en el boton rojo'}</div>
+           
+           
             <button className="button block primary" disabled={loading}>
               Click para ingresar hora de entrada
             </button>
+            <input
+          maxLength={8}
+              id="username"
+              type="text"
+              value={chekout || ''}
+              onChange={(e) => setChekout(e.target.value) }
+            />
             <label ></label>
             <button className="button block second" disabled={loading}>
               Click para ingresar hora de salida
             </button>
+            <label ></label>
+           
+           <div>Horas que en el estacionamiento: {horaPariking()} </div>
+            
           </div>
         </form>
       )}
