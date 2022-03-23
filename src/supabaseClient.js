17acfbdff7d9130fa31b135da67728012ddfb758
jesusbryan99 <91440514+jesusbryan99@users.noreply.github.com>
2022-03-22 20:41:09 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://fcrkmsjhceswscumxqlz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcmttc2poY2Vzd3NjdW14cWx6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NzczOTM3NSwiZXhwIjoxOTYzMzE1Mzc1fQ.lpuJsLpCEIfdoymiInXICrxknqYxJPFOKIpQhZxFJqo')
+export const supabase = createClient('https://fcrkmsjhceswscumxqlz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcmttc2poY2Vzd3NjdW14cWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5OTQyNzQsImV4cCI6MTk2MzU3MDI3NH0.ZbntvKBXFRy-hlkQbwSdNA8m79XF1w-0GS7KY67Hujs')
